17f476448772d2a215ec04bea267ff63abcd26b5
Patrick Dacius <Patrickdacius27@gmail.comgit config --global credential.helper store>
2024-04-06 21:35:00 -0400

    assignment check

--- src/debug.js
+++ src/debug.js
@@ -1,47 +1,74 @@
 const getRandomIntInRange = (min, max) => {
-  if (min > max) throw new Error('min must be less than max');
-  return Math.floor(Math.random() * max);
-};
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+}
+
 
 const coolnessGauge = (numOfFridges) => {
-  return numOfFridges > 5 ? 'You need more fridges.' : 'You are downright chilly!';
+  return numOfFridges >= 4 ? 'You are downright chilly!' : 'You need more fridges.';
 };
+console.log(coolnessGauge(0))
+
 
 const funkoPopAddictionLevel = (numOfFunkoPops) => {
   if (numOfFunkoPops === 0) {
     console.log('No pops? Maybe try one.');
-  } else if (numOfFunkoPops >= 1) {
+  } else if (numOfFunkoPops >= 1 && numOfFunkoPops <= 10) {
     console.log('Only a few? Keep having fun!');
-  } else if (numOfFunkoPops > 10) {
+  } else if (numOfFunkoPops >= 10 && numOfFunkoPops <= 20) {
     console.log('You have a problem.');
-  } else if (numOfFunkoPops > 20) {
+  } else if (numOfFunkoPops >= 20 && numOfFunkoPops <= 30) {
     console.log('You need help!');
-  } else {
+  } else if (numOfFunkoPops > 30) {
     console.log('You need an intervention!!!');
   }
 };
 
+funkoPopAddictionLevel(0) //no pops 
+funkoPopAddictionLevel(1)//few  
+funkoPopAddictionLevel(10)// few
+funkoPopAddictionLevel(11)// problem 
+funkoPopAddictionLevel(20)// problem 
+funkoPopAddictionLevel(21)//help
+funkoPopAddictionLevel(30)//help 
+funkoPopAddictionLevel(31)// intervention 
+funkoPopAddictionLevel(100)// intervention
+
+
+
+
+
 const getWeatherReport = (temperature) => {
+  let weatherReport;
   if (temperature > 90) {
-    const weatherReport = "It's hot and gross out.";
-    console.log(weatherReport);
+    weatherReport = "It's hot and gross out."
   } else if (temperature > 70) {
-    const weatherReport = "At least it's a dry heat.";
-    console.log(weatherReport);
+    weatherReport = "At least it's a dry heat."
+  } else if (temperature >= 32 && temperature <= 70) {
+    weatherReport = "It's not too bad!"
   } else if (temperature < 32) {
-    const weatherReport = "Wow, it's cold out.";
-    console.log(weatherReport);
+    weatherReport = "Wow, it's cold out."
   }
-  console.log("And that's your report!");
-  return weatherReport;
+
+  console.log(weatherReport);
+  console.log("And that's your report!")
+  return weatherReport
 };
 
+console.log(getWeatherReport(71));
+
+
+
 const returnPositiveNegativeZero = (num) => {
-  return num < 0
-    ? "Positive"
-    : num === 0
-      ? "Zero"
-      : "Negative";
+  if (num < 0) {
+    return "Negative"
+  } else if (num > 0) {
+    return "Positive"
+  } else if (num === 0) {
+    return "Zero"
+  }
+  console.log(returnPositiveNegativeZero(4))
 };
 
 module.exports = {
--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,17 +1,157 @@
-const measureRain = () => {
+const measureRain = (inches) => {
+  if (inches === 0) {
+    console.log('drought')
+  } else if (inches < 2) {
+    console.log('dry')
+  } else if (inches < 4) {
+    console.log('average')
+  } else if (inches < 6) {
+    console.log('rainy')
+  } else {
+    console.log('flood')
+  }
+};
+// measureRain(0) // drought
+// measureRain(1) // dry
+// measureRain(2) // average 
+// measureRain(3)// average 
+// measureRain(4)// rainy 
+// measureRain(5) // rainy 
+// measureRain(6) // flood 
+// measureRain(7) // flood 
+// measureRain(10)  // flood
+
+
+
+const happyBirthdayPet = (breed, age) => {
+  //  'snake', any age - 'Hiss hiss!'
+  //'cat', less than 5 - 'Mew mew!'
+  if (breed == "snake") {
+    console.log('Hiss hiss!')
+  } else if (breed == "cat" && age < 5) {
+    console.log('Mew mew!')
+  } else if (breed == "cat" && age >= 5) {
+    console.log('Meow meow!')
+  } else if (breed == "dog" && age < 5) {
+    console.log('Arf arf!')
+  } else if (breed == "dog" && age >= 10) {
+    console.log('Boof!')
+  } else if (breed == "dog" && age >= 5 && age <= 10) {
+    console.log('Woof woof!')
+  } else console.log('Happy birthday!')
+}
+// happyBirthdayPet("snake", 0) // hiss hiss 
+// happyBirthdayPet("snake", 4)// hiss hiss 
+// happyBirthdayPet("cat", 2)// mew mew
+// happyBirthdayPet("cat", 5)// meow 
+// happyBirthdayPet("cat", 10)// meow 
+// happyBirthdayPet("dog,", 4)//arf
+// happyBirthdayPet("dog", 5)// woof 
+// happyBirthdayPet("dog", 9)// woof 
+// happyBirthdayPet("dog", 10)/// boof
+// happyBirthdayPet("dog", 11)// boof 
+// happyBirthdayPet("bird", 1)// happy birthday 
+// happyBirthdayPet("racoon", 5)// happy birthday
+
+
+
+
+// input: any input (string, number, boolean, etc.)
+// output: string
+const funTypes = (jsType) => {
+  // we add a console.log at the top of this function to help with the flow of the function 
+  // "console.log(typeof jsType)"
+  // typeof method is used to tell us what kind of datatype is found within out if statement
+  if (typeof jsType === "string") {
+    console.log("That's just some text.")
+    // we use strict equals "===" because strict equals check for data types.
+    // from this point out the statement just continues to run 
+    // so long as one of these statements are not true. 
+  } else if (Number.isNaN(jsType)) {
+    console.log("Well, now you're just showing off.")
+  } else if (typeof jsType === "boolean") {
+    console.log("To bool, or not to bool?")
+  } else if (jsType === undefined) {
+    console.log("Nothing, but I didn't set that.")
+  } else if (jsType === null) {
+    console.log("Nothing, and I did set that.")
+  } else if (Array.isArray(jsType)) {
+    console.log("I order you to be indexed.")
+  } else if (typeof jsType === "object") {
+    console.log("Anybody got the key?")
+  } else if (typeof jsType === "number") {
+    console.log("That's a good number.")
+  }
 };
 
-const happyBirthdayPet = () => {
+// funTypes("string")
+// funTypes('');
+// funTypes(1);
+// funTypes(0);
+// funTypes(true);
+// funTypes(false);
+// funTypes(undefined);
+// funTypes(null);
+// funTypes({});
+// funTypes([]);
+// funTypes(NaN);
+
+// this isArray method will check if something is an array 
+
+// in this console log we will plug in whatever data type we would like
+//this is to catch the return message. 
+
+
+
+
+
+// console.log(typeof NaN)
+// console.log(typeof [])
+
+
+// input: float (number), string
+// output: number 
+// const rounder = (float, roundSetting) => {
+//   /* we use the method of Math.ceil() because this method helps us
+//   round up to the next interger. */
+//   if (roundSetting == 'up') {
+//     return Math.ceil(float)
+//     /*we place float within this method because float is a pramater 
+//     and the number in which float could be will continue to change
+//     */
+//   } else if (roundSetting == "down") {
+//     return Math.floor(float)
+//     /*
+// The Math.floor method is used to round down to the nearest interger.
+//     */
+//   } else if (roundSetting == "honest") {
+//     return Math.round(float)
+//   }
+//   // The Math.round method is used to return the value of a number
+//   // rounded to the nearest integer.
+//};
+const rounder = (float, roundingSetting) => {
+  if (roundingSetting === 'up') return Math.ceil(float);
+  if (roundingSetting === 'down') return Math.floor(float);
+  return Math.round(float);
 };
 
-const funTypes = () => {
-};
+console.log(rounder(1.0, "up"))
+console.log(rounder(3.5, "down"))
+console.log(rounder(3.5, "honest"))
 
-const rounder = () => {
-};
-
-const fizzBuzzish = () => {
-};
+const fizzBuzzish = (num) => {
+  if (num % 3 === 0 && num % 5 === 0) {
+    console.log("fizzBuzz!")
+  }
+  else if (num % 3 === 0) {
+    console.log("fizz")
+  }
+  else if (num % 5 === 0) {
+    console.log("buzz")
+  } else console.log(num)
+}
+// fizzBuzzish(8)
 
 module.exports = {
   measureRain,
@@ -19,4 +159,4 @@ module.exports = {
   funTypes,
   rounder,
   fizzBuzzish,
-};
+}
